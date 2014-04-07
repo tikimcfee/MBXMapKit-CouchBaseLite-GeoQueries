bfb71497a41a006e7a4603f5729c25945204be67
Ivan Lugo <ivan.lugo@sightplan.com>
2014-04-07 00:22:35 -0400

    Got CBL Models to spit out polygons with a little NSDictionary help.. but it's much less performant now... WHY.

---
+++ TheMap/SPPlaceObject.h
@@ -0,0 +1,13 @@
+//
+//  SPPlaceObject.h
+//  TheMap
+//
+//  Created by Ivan Lugo on 4/4/14.
+//  Copyright (c) 2014 Ivan Lugo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SPPlaceObject : NSObject
+
+@end
---
+++ TheMap/SPPlaceObject.m
@@ -0,0 +1,13 @@
+//
+//  SPPlaceObject.m
+//  TheMap
+//
+//  Created by Ivan Lugo on 4/4/14.
+//  Copyright (c) 2014 Ivan Lugo. All rights reserved.
+//
+
+#import "SPPlaceObject.h"
+
+@implementation SPPlaceObject
+
+@end
